1e1eb6593059518eb9b8553ea8101252d0d7d863
Paulo Gosik M. Moita <126024627+paulogosik@users.noreply.github.com>
2023-03-15 14:55:54 -0300

    Update ORDERBY.sql

--- Primeiro Módulo/ORDERBY.sql
+++ Primeiro Módulo/ORDERBY.sql
@@ -2,9 +2,9 @@ USE sakila;
 
 SELECT 
 	customer_id, 
-    customer_id * 36 AS 'RealCustomer_id',
-    amount,
-    amount - (amount * 0.1) AS '10% discount'
+	customer_id * 36 AS 'RealCustomer_id',
+	amount,
+	amount - (amount * 0.1) AS '10% discount'
 FROM payment
 WHERE customer_id < 5
 ORDER BY customer_id;
