055b204a50c84ab41ed535a430b54f1dc406b12d
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-03-16 10:36:27 -0300

    Update insertTO.sql

--- Manipulando tabelas/insertTO.sql
+++ Manipulando tabelas/insertTO.sql
@@ -1,4 +1,7 @@
 USE sakila;
 
 INSERT INTO language
-VALUES (DEFAULT, 'Portuguese', '2023-03-16 10:17:19')
+VALUES 
+	(DEFAULT, 'Polish', '2023-12-03 14:17:19'),
+    (DEFAULT, 'Spanish', '2015-06-13 12:17:19'),
+    (DEFAULT, 'Russian', '2020-03-15 10:17:19')
