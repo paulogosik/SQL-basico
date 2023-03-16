89fdf9c2ebbc27f871048e12fdf4e5ead4c94e57
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-03-16 09:53:06 -0300

    Create leftJoinRightJoin.sql

---
+++ JOIN/leftJoinRightJoin.sql
@@ -0,0 +1,14 @@
+USE sakila;
+
+SELECT 
+	cus.customer_id, 
+	cus.first_name, 
+	cus.last_name,
+    adr.address,
+    pay.rental_id,
+	pay.amount
+FROM customer cus
+JOIN payment pay 
+	ON cus.customer_id = pay.payment_id
+LEFT JOIN address adr 
+	ON cus.customer_id = adr.address_id
