fb59e568f4418d8b6b5f400f5237df4585b3758e
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-03-20 14:21:36 -0300

    Create InsertingTableCars.sql

---
+++ ProjetoSQL - Loja de Veiculos/InsertingTableCars.sql
@@ -0,0 +1,17 @@
+USE moitaveiculos;
+
+INSERT INTO moitaveiculos.cars (
+	carModel,
+    carTransmission,
+    carMotor,
+    carFuel,
+    carYear,
+    brandsID
+)
+VALUES 
+	('Polo TSI', 'Automatic', '1.0 Turbo', 'Gasoline', 2021, 4),
+    ('Urus', 'Automatic', '4.0 V8', 'Gasoline', 2022, 5),
+    ('BMW M3', 'Automatic', '3.0 Biturbo', 'Gasoline', 2020, 1),
+    ('Mustang Cobra', 'Manual', '3.0 V8', 'Gasoline', 1976, 6),
+    ('812 Superfast', 'Automatic', '6.5 V12', 'Gasoline', 2023, 2)
+    
