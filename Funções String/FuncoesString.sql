652582a146cd4ca4fae502ed87f8f22dcecb368b
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-03-20 10:21:58 -0300

    Create FuncoesString.sql

---
+++ Funções String/FuncoesString.sql
@@ -0,0 +1,28 @@
+/* 
+==================================================
+SELECT TRIM(BOTH 'a' FROM 'aaaaaPauloaaaaa') AS TRIM
+#SELECT TRIM('      Paulo       ') AS TRIM
+#SELECT LTRIM('      Paulo       ') AS TRIM
+#SELECT RTRIM('      Paulo       ') AS TRIM
+
+A função TRIM() serve para tirar espaços ou letras repetidas.
+===================================================
+*/
+
+/* 
+==================================================
+SELECT LOCATE('a', 'Paulo')
+
+A função LOCATE() funciona para localizar uma string dentro de uma palavra, te retorna a posição.
+===================================================
+*/
+
+/*
+Outras funções: 
+	UCASE()
+    LCASE()
+    REPEAT()
+    LENGTH()
+    RIGHT()
+    LEFT()
+*/
